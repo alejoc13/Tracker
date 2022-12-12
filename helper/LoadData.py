2d319272807b2258a3214195d1d2cc2a66e3635d
rodrij545 <rodrij545@medtronic.com>
2022-12-12 16:30:50 -0500

    Modificación para agregar una columna al submission plan

--- helper/LoadData.py
+++ helper/LoadData.py
@@ -160,7 +160,7 @@ def uploadData():
 
 def load_SPlan():
     print('Cargando Submission Plan...')
-    df_plan = pd.read_excel('Documents\Submission Plan - Full Report.xlsx',usecols=['Id','RAS Name','Project/Product Name','Status','Submission Type','Submission Date','Approval Date','Therapy Group',
+    df_plan = pd.read_excel('Documents\Submission Plan - Full Report.xlsx',usecols=['Id','RAS Name','Project/Product Name','Status','Submission Type','Expected Submission Date','Approval Date','Therapy Group',
                             'Expected Approval Date','Submission Date','Country','Cluster','License Number'])
     df_plan = df_plan.rename(columns={'Project/Product Name':'PRODUCT NAME','License Number':'REGISTRATION NUMBER'})
     print('Submission Plan cargado')
